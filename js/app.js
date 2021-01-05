8456d2f24881708792e159753fc5e9a4edb22f09
David Travezano <Davidu.0622@gmail.com>
2021-01-05 16:14:05 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(window).load(function() {
+		$(".se-pre-con").fadeOut("slow");;
+	});
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+
+	$('#maquinas').load(efectomaquina);	
+});
+function escribir(){
+		document.getElementById("maquinas").innerText= p.substr(0,num_c)+ "|";
+		num_c++;
+		if (num_c<=p.length){
+		setTimeout("escribir()", 100);
+		}
+	}
+	function efectomaquina() {
+		p="<David Travezaño />";
+		num_c=0;
+		escribir();
+	}
+
+	setTimeout("efectomaquina()",1000);
